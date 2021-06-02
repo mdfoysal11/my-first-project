75f839e2644f277bb85ea5ed081d4ceff5641479
Md. Foysal <83161665+mdfoysal11@users.noreply.github.com>
2021-06-02 22:17:35 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<?php wp_head();?>
+</head>
+<body <?php body_class();?>>
