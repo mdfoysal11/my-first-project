7bdc4c5de843a6cc2e21c97ca6c06c5ad50e9521
alamin-php <alamin.php100@gmail.com>
2021-06-04 02:37:38 +0600

    added version control system added

--- functions.php
+++ functions.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+if(site_url() == "http://demo.lwhh.com"){
+    define("VERSION", time());
+}else{
+    define("VERSION", wp_get_theme() -> get("Version"));
+}
+
 function first_theme()
 {
 	load_theme_textdomain("wpproject");
@@ -7,9 +13,9 @@ function first_theme()
 add_action( "aftar_setup_theme", "first_theme");
 
 function all_link() {
-    wp_enqueue_style( "bootstrap", get_theme_file_uri( "/assets/css/bootstrap.min.css" ), null,time());
-    wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, time());
-    wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), time(), true );
+    wp_enqueue_style( "bootstrap", get_theme_file_uri( "/assets/css/bootstrap.min.css" ), null,VERSION);
+    wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, VERSION);
+    wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), VERSION, true );
     wp_enqueue_style( "style-css", get_stylesheet_uri() );
 }
 add_action("wp_enqueue_scripts","all_link");
