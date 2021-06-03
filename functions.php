cfbadf448dcf80f74f601c56359f3a0692d58e11
alamin-php <alamin.php100@gmail.com>
2021-06-04 02:49:05 +0600

    updated title-tage and function name

--- functions.php
+++ functions.php
@@ -5,17 +5,18 @@ if(site_url() == "http://demo.lwhh.com"){
     define("VERSION", wp_get_theme() -> get("Version"));
 }
 
-function first_theme()
+function wpproject_bootstraping()
 {
 	load_theme_textdomain("wpproject");
-	add_theme_support("tiyle-tag");
+	add_theme_support( "title-tag");
 }
-add_action( "aftar_setup_theme", "first_theme");
+add_action( "after_setup_theme", "wpproject_bootstraping");
 
-function all_link() {
+function wpproject_assets() {
     wp_enqueue_style( "bootstrap", get_theme_file_uri( "/assets/css/bootstrap.min.css" ), null,VERSION);
     wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, VERSION);
-    wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), VERSION, true );
     wp_enqueue_style( "style-css", get_stylesheet_uri() );
+    
+    wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), VERSION, true );
 }
-add_action("wp_enqueue_scripts","all_link");
+add_action("wp_enqueue_scripts","wpproject_assets");
--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
 	<?php wp_head();?>
 </head>
 <body <?php body_class();?>>
