a5aa212edaca375cb00ce323cf46587fef023cd7
Md. Foysal <83161665+mdfoysal11@users.noreply.github.com>
2021-06-04 20:34:35 +0600

    footer html added

--- footer.php
+++ footer.php
@@ -1,3 +1,50 @@
-	<?php wp_footer() ?>
+<footer class="footer">
+	 <div class="contairne">
+	 	<div class="row">
+	 		<div class="col-md-3">
+	 			<h4>part 1</h4>
+	 			<ul>
+	 				<li><a href="#">home</a></li>
+	 				<li><a href="#">about</a></li>
+	 				<li><a href="#">service</a></li>
+	 				<li><a href="#">blog</a></li>
+	 			</ul>
+	 		</div>
+	 		<div class="col-md-3">
+	 			<h4>part 2</h4>
+	 			<ul>
+	 				<li><a href="#">blog</a></li>
+	 				<li><a href="#">home</a></li>
+	 				<li><a href="#">service</a></li>
+	 				<li><a href="#">about</a></li>
+	 			</ul>
+	 		</div>
+	 		<div class="col-md-3">
+	 			<h4>part 3</h4>
+	 			<ul>
+	 				<li><a href="#">home</a></li>
+	 				<li><a href="#">service</a></li>
+	 				<li><a href="#">about</a></li>
+	 				<li><a href="#">blog</a></li>
+	 			</ul>
+	 		</div>
+	 		<div class="col-md-3">
+	 			<h4>icon part</h4>
+	 			
+	 			<div class="icon">
+	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+	 			
+	 				
+	 			</div>
+	 		</div>
+
+	 	</div>
+	 </div>
+</footer>
+
+<?php wp_footer() ?>
+
 </body>
 </html>
