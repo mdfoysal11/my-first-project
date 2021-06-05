aa9519bb1ba6eb77b788d0a07266ea11bff9740f
alamin <dev.alaminsarker@gmail.com>
2021-06-05 16:24:42 +0600

    added post markup

--- index.php
+++ index.php
@@ -2,4 +2,65 @@
 <?php get_template_part( "theme-parts/common/navigation"); ?>
 <?php get_template_part( "theme-parts/front-page/hero"); ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="posts">
+                <div class="post-img">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri();?>/assets/images/wpproject_post1.jpg" alt="Post iamge">
+                </div>
+                <div class="post-content">
+                    <h4 class="post-title"><a href="#">Lorem ipsum dolor sit amet.</a></h4>
+                    <div class="post-meta">
+                        <p>Author: Foysal | Post At: 12th May, 2021</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium porro sunt quidem cum provident culpa asperiores.</p>
+                </div>
+            </div>
+        </div>        
+        <div class="col-md-3">
+            <div class="posts">
+                <div class="post-img">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri();?>/assets/images/wpproject_post2.jpg" alt="Post iamge">
+                </div>
+                <div class="post-content">
+                    <h4 class="post-title"><a href="#">Lorem ipsum dolor sit amet.</a></h4>
+                    <div class="post-meta">
+                        <p>Author: Foysal | Post At: 12th May, 2021</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium porro sunt quidem cum provident culpa asperiores.</p>
+                </div>
+            </div>
+        </div>        
+        <div class="col-md-3">
+            <div class="posts">
+                <div class="post-img">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri();?>/assets/images/wpproject_post1.jpg" alt="Post iamge">
+                </div>
+                <div class="post-content">
+                    <h4 class="post-title"><a href="#">Lorem ipsum dolor sit amet.</a></h4>
+                    <div class="post-meta">
+                        <p>Author: Foysal | Post At: 12th May, 2021</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium porro sunt quidem cum provident culpa asperiores.</p>
+                </div>
+            </div>
+        </div>        
+        <div class="col-md-3">
+            <div class="posts">
+                <div class="post-img">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri();?>/assets/images/wpproject_post2.jpg" alt="Post iamge">
+                </div>
+                <div class="post-content">
+                    <h4 class="post-title"><a href="#">Lorem ipsum dolor sit amet.</a></h4>
+                    <div class="post-meta">
+                        <p>Author: Foysal | Post At: 12th May, 2021</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium porro sunt quidem cum provident culpa asperiores.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(  ) ?>
