eb9cebdcea6e71c794d95fba92791e12a6a097a5
alamin <dev.alaminsarker@gmail.com>
2021-06-06 09:48:04 +0600

    added mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+; (function($){
+	$(document).ready(function(){
+		$("#menu-top-menu").slicknav()
+	});
+})(jQuery);
--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-if(site_url() == "http://demo.lwhh.com"){
+if(site_url() == "http://demo.themedev.com"){
     define("VERSION", time());
 }else{
     define("VERSION", wp_get_theme() -> get("Version"));
@@ -18,9 +18,11 @@ function wpproject_assets() {
     wp_enqueue_style( "google-fonts", "//fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Lobster&display=swap");
     wp_enqueue_style( "fontawesome", "//use.fontawesome.com/releases/v5.15.3/css/all.css");
     wp_enqueue_style( "bootstrap", get_theme_file_uri( "/assets/css/bootstrap.min.css" ), null,VERSION);
+    wp_enqueue_style( "slicknav-css", get_theme_file_uri( "/assets/css/slicknav.min.css" ), null, VERSION);
     wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, VERSION);
     wp_enqueue_style( "style-css", get_stylesheet_uri() );
     
+    wp_enqueue_script("slicknav-js", get_theme_file_uri("/assets/js/jquery.slicknav.min.js"), array("jquery"), VERSION, true );
     wp_enqueue_script("main-js", get_theme_file_uri("/assets/js/main.js"), array("jquery"), VERSION, true );
 }
 add_action("wp_enqueue_scripts","wpproject_assets");
